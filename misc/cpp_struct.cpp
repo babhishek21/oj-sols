c188accb97c5b7049c0b750c34cc41c9f230cc16
Abhishek Bhattacharya <babhishek.bhattacharya@gmail.com>
2016-06-18 00:27:57 +0530

    Added CTCI ch2 (in progress)

--- misc/cpp_struct.cpp
+++ misc/cpp_struct.cpp
@@ -1,10 +1,36 @@
+#include <bits/stdc++.h> // using GCC/G++
+using namespace std;
+
+// sample linked list node struct
+typedef struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x): val(x), next(nullptr) {};
+} ListNode;
+
+int main() {
+  ListNode *head = new ListNode(3), *curr;
+  head->next = new ListNode(5);
+  head->next->next = new ListNode(4);
+
+  curr = head;
+  cout << "HEAD->";
+  while(curr) {
+    cout << curr->val << "->";
+    curr = curr->next;
+  }
+  cout << "NULL" << endl;
+
+  return 0;
+}
+
+/*
+
 // C -style structs can behave like objects in C++
 struct lol {
   int lolval;
-
   lol(int n): lolval(n) {} // this is apparantly a friggin' constructor
-                          // works like a class less object. WTF?
-};
+};                         // works like a class less object. WTF?
 
 // make a new instance of lol
 lol myNewLol(3); // this works in C++
@@ -13,10 +39,10 @@ lol myNewLol(3); // this works in C++
 // Make a friggin' pointer
 lol *myNewLol = new lol(3);
 
-// [lang='hi'] bilkul object syntax bana diya.
-// [lang='hi'] DO alag alag syntax kaun rakhta hian? hadd hian :/
+[lang='hi'] bilkul object syntax bana diya.
+[lang='hi'] DO alag alag syntax kaun rakhta hian? hadd hian :/
 
-///////////////// After you are tired of this bullshit, let's see JAVA ////////
+/////////////// After you are tired of this bullshit, let's see JAVA ////////
 
 class lol {
   private int lolval;
@@ -31,3 +57,4 @@ class lol {
   }
 }
 
+*/
---
+++ misc/ctci_indian_6th_ed/ch2_linked_lists.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h> // using GCC/G++
+using namespace std;
+
+// linked list node class
+class Node {
+public:
+  int val;
+  Node *next;
+
+  Node(int x) {
+    val = x;
+    next = nullptr;
+  }
+};
+
+/**
+ * 2.1 Remove Duplicates
+ * O(N^2) time algorithm
+ */
+void removeDuplicates(Node *head) {
+  Node *curr = head;
+
+  while(curr->next) {
+    Node *scout = curr;
+
+    while(scout->next) {
+      if(scout->next->val == curr->val) {
+        scout->next = scout->next->next;
+      }
+      scout = scout->next;
+    }
+
+    curr = curr->next;
+  }
+}
+/*
+ * Alternate approach with Hash Table/Set
+ * O(n) run time + O(n) extra space
+ */
+void removeDuplicatesSet(Node *head) {
+  unordered_set<int> HashSet;
+  Node *curr = head;
+  HashSet.insert(curr->val);
+
+  while(curr->next) {
+    if(HashSet.find(curr->next->val) != HashSet.end())
+      curr->next = curr->next->next;
+    else
+      HashSet.insert(curr->next->val);
+
+    curr = curr->next;
+  }
+}
+
+/**
+ * 2.2 Kth to Last Element
+ * O(n) algorithm with two pointers
+ */
+Node* kthToLast(Node *head, unsigned int k) {
+  Node *curr = head, *ret = nullptr;
+  unsigned int count = 0;
+
+  while(curr) {
+    count++;
+
+    if(count >= k) {
+      if(count == k) ret = head;
+      ret = ret->next;
+    }
+
+    curr = curr->next;
+  }
+
+  return ret;
+}
+
+/**
+ * 2.3 Delete Middle Node
+ * O(1) time algorithm. Not really
+ * deleting the node, just overwriting.
+ */
+void deleteMiddleNode(Node *node) {
+  Node *temp = node->next;
+  node->val = temp->val;
+  node->next = temp->next;
+  delete temp; // No GC in C++
+}
+
+/**
+ * 2.4 Partition List
+ * O(n) time algo with O(n) extra space
+ * This paritioning is unstable.
+ */
+Node* partitionList(Node *head, int pivot) {
+  Node *newHead = head, *newTail = head, *curr=head;
+
+  while(curr) {
+    Node *temp = curr;
+    if(curr->val < pivot) {
+      curr->next = newHead;
+      newHead = curr;
+    } else {
+      newTail->next = curr;
+      newTail = newTail->next;
+    }
+    curr = temp->next;
+  }
+
+  newTail->next = nullptr;
+  return newHead;
+}
+
+
+
+int main() {
+
+
+  return 0;
+}
